1ca6873b63e1c8cebbc631cb7b61c809daeb64ba
lzwDT <lzw694476922@gmail.com>
2022-09-02 20:43:46 +0800

    day1_1

--- test_tensorboard.py
+++ test_tensorboard.py
@@ -3,7 +3,7 @@ from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter("logs")
 
 for i in range(100):
-    writer.add_scalar("y=x",scalar_value=2*i,global_step=i)
+    writer.add_scalar("y=x",scalar_value=3*i,global_step=i)
 
 writer.close()
 
